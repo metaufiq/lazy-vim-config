b3d2d8fd344b71c8013d534f3431b4b05df1b64b
Muhammad Taufiqulsadi <mtaufiqulsadi@gmail.com>
2024-03-08 23:33:42 +0700

    add new config for lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
-map("n", "<space>ct", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "show lsp type buffer" })
+map("n", "<space>cct", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "show lsp type buffer" })
